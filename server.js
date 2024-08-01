6c3de752ea2383c4236b3e55f84f25fb83329739
SkyActually <jimmiesmith1222@gmail.com>
2024-08-01 18:09:11 -0500

    made basic backend express app

---
+++ server.js
@@ -0,0 +1,16 @@
+
+require("dotenv").config();
+
+const { PORT } = process.env;
+
+const express = require("express");
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
