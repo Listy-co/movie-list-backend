eca26b502472b7dd6c5b51cdfbc2082619cb312f
SumayaSaidSelma <sumayasaidselma@gmail.com>
2024-08-09 20:29:04 -0400

    updateserver

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require('cors');
+const mongoose = require("mongoose");
 const Movie = require('./models/Movie');
 const movieRouter = require("./routes/MovieRoutes"); //import my routs files 
 const { PORT } = process.env;
